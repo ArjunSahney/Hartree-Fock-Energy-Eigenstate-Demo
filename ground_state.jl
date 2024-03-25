6be322876e45cbb3de1d63192ccd0cea6adce063
ArjunSahney <115757328+ArjunSahney@users.noreply.github.com>
2024-03-25 19:36:12 -0400

    Update ground_state.jl

--- ground_state.jl
+++ ground_state.jl
@@ -2,41 +2,34 @@ using Quiqbox
 
 # Define the nuclear configuration of the H2 molecule
 nuc = ["H", "H"]
-bond_length = 3.0  
+bond_length = 3.0 # Angstrom
 nucCoords = [[-bond_length/2, 0.0, 0.0], [bond_length/2, 0.0, 0.0]]
 
-# Generate basis set
+# Generate the basis set for the H2 molecule
 bs = genBasisFunc.(nucCoords, "STO-3G", nuc) |> flatten
 
-# Mark params for optimization
-pars = markParams!(bs, true)
+# Run the RHF calculation
+resRHF = runHF(bs, nuc, nucCoords)
 
-# Optimize the marked parameters
-isConverged, Es = optimizeParams!(pars, bs, nuc, nucCoords, POconfig(maxStep=10))
+# Print the RHF results
+println("RHF Results:")
+println("Hartree–Fock Energy: ", resRHF.Ehf)
+println("Coefficient Matrix: ", resRHF.C)
+println("Orbital Energies: ", resRHF.Eo)
+println("Occupation Numbers: ", resRHF.occu)
 
-# Run the RHF calculation with the optimized basis set
-resRHF_opt = runHF(bs, nuc, nucCoords)
+# Define the UHF configuration
+HFc_UHF = HFconfig(HF=:UHF)
 
-# Print the RHF results with the optimized basis set
-println("RHF Results (Optimized Basis Set):")
-println("Hartree–Fock Energy: ", resRHF_opt.Ehf)
-println("Coefficient Matrix: ", resRHF_opt.C)
-println("Orbital Energies: ", resRHF_opt.Eo)
-println("Occupation Numbers: ", resRHF_opt.occu)
+# Run the UHF calculation
+resUHF = runHF(bs, nuc, nucCoords, HFc_UHF)
 
-# Define the SCF configuration for UHF
-SCFc = SCFconfig()
-HFc = HFconfig(HF=:UHF, C0=:GWH, SCF=SCFc)
+# Print the UHF results
+println("\nUHF Results:")
+println("Hartree–Fock Energy: ", resUHF.Ehf)
+println("Coefficient Matrix: ", resUHF.C)
+println("Orbital Energies: ", resUHF.Eo)
+println("Occupation Numbers: ", resUHF.occu)
 
-# Run the UHF calculation with the optimized basis set
-resUHF_opt = runHF(bs, nuc, nucCoords, HFc)
-
-# Printing UHF results
-println("\nUHF Results (Optimized Basis Set):")
-println("Hartree–Fock Energy: ", resUHF_opt.Ehf)
-println("Coefficient Matrix: ", resUHF_opt.C)
-println("Orbital Energies: ", resUHF_opt.Eo)
-println("Occupation Numbers: ", resUHF_opt.occu)
-
-# Create a MatterByHF object in case you need further data processing
-mol_opt = MatterByHF(resRHF_opt)
+# Create a MatterByHF object for further data processing
+mol = MatterByHF(resRHF)
